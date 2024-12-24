434298128e7cb141498d8ff82cf074595713d880
kagemaru <69294124+kagemarus@users.noreply.github.com>
2024-12-25 01:14:05 +0700

    Add files via upload

---
+++ Project1.cpp
@@ -0,0 +1,20 @@
+﻿int main()
+{
+	setlocale(LC_ALL, "");
+	int a;
+	int b;
+	std::cout << "Введите число A:";
+	std::cin >> a;
+	std::cout << "Введите число B:";
+	std::cin >> b;
+	if (b % a == 0)
+	{
+		std::cout << "Число " << a << " является делителем числа " << b <<
+			std::endl;
+	}
+	else
+	{
+		std::cout << "Число " << a << " не является делителем числа " << b <<
+			std::endl;
+	}
+}
