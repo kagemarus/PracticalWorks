7cc65e4062fe00f02b91cbd9dda49bde4c954e95
kagemaru <69294124+kagemarus@users.noreply.github.com>
2024-12-25 01:38:20 +0700

    Delete Project5.cpp

--- Project5.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-int main()
-{
- setlocale(LC_ALL, "");
- double meters;
- std::cout << "Введите расстояние в метрах: ";
- std::cin >> meters;
- std::cout << "Distance in centimeters: " << meters * 100 << std::endl;
-}
