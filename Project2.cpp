e65954aa00cdbf8f004f1311ef9e2fde7b40d620
kagemaru <69294124+kagemarus@users.noreply.github.com>
2024-12-25 01:30:41 +0700

    Add files via upload

---
+++ Project2.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+
+int main()
+{
+    setlocale(LC_ALL, "");
+    int m, n;
+
+    std::cout << "Введите число m: ";
+    std::cin >> m;
+    std::cout << "Введите число n: ";
+    std::cin >> n;
+
+    if (m > n)
+        std::cout << "Number " << m << " > " << n << std::endl;
+    else if (m < n)
+        std::cout << "Number " << m << " < " << n << std::endl;
+    else
+        std::cout << "The numbers are equal" << std::endl;
+}
