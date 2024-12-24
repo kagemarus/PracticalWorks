48caef1549e41a926fb74ea3e9f5a1357f0c9499
kagemaru <69294124+kagemarus@users.noreply.github.com>
2024-12-25 01:39:30 +0700

    Add files via upload

---
+++ Project5.cpp
@@ -0,0 +1,9 @@
+﻿#include <iostream>
+int main()
+{
+	setlocale(LC_ALL, "");
+	double meters;
+	std::cout << "Введите расстояние в метрах: ";
+	std::cin >> meters;
+	std::cout << "Distance in centimeters: " << meters * 100 << std::endl;
+}
