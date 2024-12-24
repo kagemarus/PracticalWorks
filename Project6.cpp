12c47fb6a1b83563b39e652519d35faac39d7f25
kagemaru <69294124+kagemarus@users.noreply.github.com>
2024-12-25 01:35:01 +0700

    Add files via upload

---
+++ Project6.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+int main()
+{
+	setlocale(LC_ALL, "");
+	double kilometers, meters;
+	std::cout << "Введите расстояние в километрах: ";
+	std::cin >> kilometers;
+	std::cout << "Введите расстояние в метрах: ";
+	std::cin >> meters;
+	if (kilometers * 1000 < meters)
+		std::cout << "Наименьшее расстояние: " << kilometers * 1000 << " метров" <<
+		std::endl;
+	else
+		std::cout << "Наименьшее расстояние: " << meters << " метров" << std::endl;
+}
