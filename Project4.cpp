f362cebac688713436b0c1598811fe0a66975d8c
kagemaru <69294124+kagemarus@users.noreply.github.com>
2024-12-25 01:32:41 +0700

    Add files via upload

---
+++ Project4.cpp
@@ -0,0 +1,9 @@
+﻿#include <iostream>
+int main()
+{
+	setlocale(LC_ALL, "");
+	int radius;
+	std::cout << "Введите радиус окружности: ";
+	std::cin >> radius;
+	std::cout << "Диаметр окружности: " << radius * 2 << std::endl;
+}
